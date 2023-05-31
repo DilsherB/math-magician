f0f75653e24ae7d999b716d384908bcac03cd1a1
Dilsher <dilsherb@gmail.com>
2023-05-31 16:09:16 +0300

    Calculator functionality applied.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,35 @@
-import React from "react";
-import Child from "./child";
+import { useState } from "react";
+import calculate from "../logic/calculate";
 
 const Calculator = () => {
+  const [state, setState] = useState({ next: null, operation: null, total: 0 });
+  const btnClickHandler = (e) => {
+    const value = e.target.textContent;
+    const res = calculate(state, value);
+    setState((prevState) => ({ ...prevState, ...res }));
+  };
+  const btns = () => {
+    const calcBtns = [];
+    // prettier-ignore
+    const btnArr = ["AC", "+/-", "%", "÷", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", "0", ".", "="];
+    btnArr.forEach((btn) => {
+      return calcBtns.push(
+        <button type="button" key={btn} id={btn} onClick={btnClickHandler}>
+          {btn}
+        </button>
+      );
+    });
+    return calcBtns;
+  };
   return (
-    <div className="display">
-      <div className="result">0</div>
-      <div className="calculator">
-        <Child />
+    <div className="calculator">
+      <div className="display">
+        {state.total}
+        {state.operation}
+        {state.next}
       </div>
+      <div className="btnsContainer">{btns()}</div>
     </div>
   );
 };
-
 export default Calculator;
