456ae1bac0c18a97ee00f8469d754c224b8060ed
Dilsher <dilsherb@gmail.com>
2023-06-01 13:06:37 +0300

    useEffect hook introduced

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+const Quote = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      
+    };
+    fetchData();
+  });
+  return <div><h1>Hi</h1></div>;
+};
+
+export default Quote;
