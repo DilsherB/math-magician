0c0d5051843f71a373af98c6059f0e88f1436b58
Dilsher Balouch <dilsherb@gmail.com>
2023-06-01 17:13:32 +0300

    quote component completed.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,15 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Quote = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
-      const apiData = await fetch("https://api.api-ninjas.com/v1/quotes?category=education");
-      const data = await apiData.json();
-      console.log(data);
+      try {
+        const apiData = await fetch(
+          "https://api.api-ninjas.com/v1/quotes?category=inspirational",
+          {
+            headers: {
+              "X-Api-Key": "eqaKx38o0Fg1f/rZqOA/sg==epV9zWMwWAegkqnF",
+            },
+          }
+        );
+        const res = await apiData.json();
+        setData(res[0]);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
     };
     fetchData();
-  });
-  return <div><h1>Hi</h1></div>;
+  }, []);
+  return (
+    <div className="quotes">
+      {loading && <div className="center">Loading...</div>}
+      {error && <div>{error}</div>}
+      {data && (
+        <div>
+          <p>{data.quote}</p>
+          <h3 className="right">~{data.author}</h3>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Quote;
