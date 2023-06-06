b1f363ecd7a645a19530c4644a2ef0a542e45e07
Dilsher <dilsherb@gmail.com>
2023-06-06 16:39:40 +0300

    Home page added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Calculator from "./components/Calculator";
+import Home from "./components/Home";
 import Quote from "./components/Quote";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
+      <Menu />
+      <Home />
       <Calculator />
       <Quote />
     </div>
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <pre className="home">
+      Hi, Guest:
+      <p>
+        Welcome to my Math Magician Web App. Here I have made it using React,
+        bootStrap, React Icons, CSS and React Router Dom.
+        Here you can do basic calculations by going to calculator tab from the menu.
+        You can also view a random quote by going to the quote tab from the menu.
+      </p>
+    </pre>
+  );
+};
+
+export default Home;
