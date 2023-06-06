a4e78b1fd76ade27a0ea574d873e7c5ff7a7d311
Dilsher <dilsherb@gmail.com>
2023-06-06 16:49:59 +0300

    NavBar added.

---
+++ src/components/Menu.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Menu = () => {
+  const menuItems = {
+    home: "Home",
+    calculator: "Calculator",
+    quote: "Quote",
+  };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <h3>Math Magicians</h3>
+        {/* <div className="collapse navbar-collapse" id="navbarNav"> */}
+        <ul className="navbar-nav">
+          {Object.keys(menuItems).map((item) => (
+            <li className="nav-item" key={item}>
+              <a className="nav-link" href={`#${item}`}>
+                {menuItems[item]}
+              </a>
+            </li>
+          ))}
+        </ul>
+        {/* </div> */}
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
