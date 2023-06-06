33f86b98edb18ecd34a7efc72a01bddfc45281f7
Dilsher <dilsherb@gmail.com>
2023-06-06 16:58:28 +0300

    made some changes in navbar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,16 +9,7 @@ const Navbar = () => {
   };
   return (
     <div className="d-inline-flex px-3 w-100">
-      <a href="#home" className="text-decoration-none">
-        <img
-          src="https://img.freepik.com/premium-vector/hotel-logo-simple-illustration_434503-736.jpg?w=200"
-          alt="Logo"
-          width={40}
-          height={40}
-          className="rounded-circle mt-2"
-        />
-      </a>
-      <h2 className="mt-2 col-2">Math Magicians</h2>
+      <h2>Math Magicians</h2>
       <ul className="d-flex justify-content-end gap-5 w-100 mt-2">
         {Object.keys(menuItems).map((item) => (
           <li key={item} className="list-group-item">
