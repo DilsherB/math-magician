47edd4621bbbd5fe08a6ba57ff7fdc56bfba1b2e
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-06-08 19:49:26 +0630

    Fixed eslint errors

--- src/logic/test/calculate.test.js
+++ src/logic/test/calculate.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
 import operate from "./operate";
 import calculate from "./calculate";
 
--- src/logic/test/operate.test.js
+++ src/logic/test/operate.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
 import Big from "big.js";
 import operate from "./operate";
 
